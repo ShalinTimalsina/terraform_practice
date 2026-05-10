e3e2b5d71e52eedd124ebc88d701e283225c0121
ShalinTimalsina <shalintimalsina12@gmail.com>
2026-05-10 22:35:03 +0545

    Updated the Static Website

--- static-website-project/output-variables.tf
+++ static-website-project/output-variables.tf
@@ -1,3 +1,7 @@
 output "static_webapp_bucket_name" {
     value = aws_s3_bucket.my_static_webapp.bucket
 }
+
+output "Static_website_url" {
+    value = aws_s3_bucket_website_configuration.Static_hosting.website_endpoint
+}
--- static-website-project/resources.tf
+++ static-website-project/resources.tf
@@ -1,21 +1,67 @@
 resource "random_id" "s3_bucket_random_value" {
-    byte_length = 4
-  
+  byte_length = 4
+
 }
 
 resource "aws_s3_bucket" "my_static_webapp" {
   bucket = "shalin-static-project-${random_id.s3_bucket_random_value.hex}"
 }
 
-resource "aws_s3_object" "index_file" {
-    bucket = aws_s3_bucket.static_website_project_bucket.bucket
-    key = "index.html"
-    source = "./index.html"  
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.my_static_webapp.bucket
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
 }
 
 
-resource "aws_s3_object" "css_files" {
-    bucket = aws_s3_bucket.static_website_project_bucket.bucket
-    key = "css_files"
-    source = "./css"  
+resource "aws_s3_bucket_policy" "Bucket_policy" {
+  bucket = aws_s3_bucket.my_static_webapp.bucket
+
+  depends_on = [
+    aws_s3_bucket_public_access_block.public_access
+  ]
+
+  policy = jsonencode(
+    {
+      Version = "2012-10-17",
+      Statement : [
+        {
+          Sid       = "PublicReadGetObject",
+          Effect    = "Allow",
+          Principal = "*",
+          Action    = "s3:GetObject",
+          Resource  = "${aws_s3_bucket.my_static_webapp.arn}/*"
+
+
+        }
+      ]
+    }
+  )
+}
+
+resource "aws_s3_bucket_website_configuration" "Static_hosting" {
+  bucket = aws_s3_bucket.my_static_webapp.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
+
+}
+
+resource "aws_s3_object" "index_file" {
+  bucket       = aws_s3_bucket.my_static_webapp.bucket
+  key          = "index.html"
+  source       = "./index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "css_file" {
+  bucket       = aws_s3_bucket.my_static_webapp.bucket
+  key          = "styles.css"
+  source       = "./styles.css"
+  content_type = "text/css"
 }
